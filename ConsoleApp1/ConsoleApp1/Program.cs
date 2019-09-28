a8ced72f650eeab04d1aef85efada07aa2d5c450
student <student@DESKTOP-OT59EC7>
2019-09-28 14:41:32 +0200

    P3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ namespace Programowanie3
 {
     class Program
     {
-        static int iloscProduktow = 0;
+        static int produktPierwszy = 0, produktDrugi = 0, produktTrzeci = 0;
         static void wozek()
         {
             int odleglosc = -1, licznik = 0, sumaOdleglosci = 0;
@@ -22,18 +22,47 @@ namespace Programowanie3
 
             Console.WriteLine("Wynik to " + sredniaOdleglosc);
         } //28.09
-        static string UnikalnaNazwa() //28.09
+        static void UnikalnaNazwa(string nazwaProduktu) //28.09
         {
-            return "Produkt " + iloscProduktow++;
+            switch (nazwaProduktu)
+            {
+                case "1":
+                    Console.WriteLine("Produkt [" + nazwaProduktu + "] " + produktPierwszy++);
+                    break;
+                case "2":
+                    Console.WriteLine("Produkt [" + nazwaProduktu + "] " + produktDrugi++);
+                    break;
+                case "3":
+                    Console.WriteLine("Produkt [" + nazwaProduktu + "] " + produktTrzeci++);
+                    break;
+                default:
+                    break;
+            }
         }
         static void Main(string[] args)
         {
             //wozek();
             string escape = "exit";
+            string wybor = "";
 
-            while(Console.ReadLine() != escape)
+            while (!wybor.Equals(escape))
             {
-                Console.WriteLine(UnikalnaNazwa());
+                wybor = Console.ReadLine();
+
+                switch (wybor)
+                {
+                    case "1":
+                        UnikalnaNazwa("1");
+                        break;
+                    case "2":
+                        UnikalnaNazwa("2");
+                        break;
+                    case "3":
+                        UnikalnaNazwa("3");
+                        break;
+                    default:
+                        break;
+                }
             }
 
         }
