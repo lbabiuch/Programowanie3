42b11dcac63346ec8116db023e7d9807bb71875d
student <student@DESKTOP-OT59EC7>
2019-09-28 13:57:08 +0200

    Drugie podejscie

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Programowanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int odleglosc = -1, licznik = 0;
+
+            float sredniaOdleglosc = 0;
+
+            string odpowiedz = Console.ReadLine();
+            odleglosc = Convert.ToInt32(odpowiedz);
+
+        }
+    }
+}
