37dad0fe1563561d8e85151cfb8785c325d81a0f
student <student@DESKTOP-OT59EC7>
2019-09-28 14:02:39 +0200

    TEST

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,12 +6,20 @@ namespace Programowanie3
     {
         static void Main(string[] args)
         {
-            int odleglosc = -1, licznik = 0;
-
+            int odleglosc = -1, licznik = 0, sumaOdleglosci = 0;
             float sredniaOdleglosc = 0;
 
-            string odpowiedz = Console.ReadLine();
-            odleglosc = Convert.ToInt32(odpowiedz);
+            while (odleglosc != 0)
+            {
+                string odpowiedz = Console.ReadLine();
+                odleglosc = Convert.ToInt32(odpowiedz);
+                sumaOdleglosci += odleglosc;
+                licznik++;
+            }
+
+            sredniaOdleglosc = (float)sumaOdleglosci / (licznik - 1);
+
+            Console.WriteLine("Wynik to " + sredniaOdleglosc);
 
         }
     }
