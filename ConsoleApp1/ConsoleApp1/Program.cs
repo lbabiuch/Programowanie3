664d5b7aaf413e0e42042e8706c8f48d13c58b2b
student <student@DESKTOP-OT59EC7>
2019-09-28 14:23:54 +0200

    P3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,8 @@ namespace Programowanie3
 {
     class Program
     {
-        static void Main(string[] args)
+        static int iloscProduktow = 0;
+        static void wozek()
         {
             int odleglosc = -1, licznik = 0, sumaOdleglosci = 0;
             float sredniaOdleglosc = 0;
@@ -20,6 +21,20 @@ namespace Programowanie3
             sredniaOdleglosc = (float)sumaOdleglosci / (licznik - 1);
 
             Console.WriteLine("Wynik to " + sredniaOdleglosc);
+        } //28.09
+        static string UnikalnaNazwa() //28.09
+        {
+            return "Produkt " + iloscProduktow++;
+        }
+        static void Main(string[] args)
+        {
+            //wozek();
+            string escape = "exit";
+
+            while(Console.ReadLine() != escape)
+            {
+                Console.WriteLine(UnikalnaNazwa());
+            }
 
         }
     }
